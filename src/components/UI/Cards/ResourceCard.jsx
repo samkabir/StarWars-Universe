aadcd2fa8b5dd9f8cdc493032cae1611d2d169db
Samiul Kabir <samkabir26@gmail.com>
2025-05-29 11:40:19 +0600

    cards

--- src/components/UI/Cards/ResourceCard.jsx
+++ src/components/UI/Cards/ResourceCard.jsx
@@ -5,7 +5,7 @@ const ResourceCard = ({ resource, onClick, className = '', resourceType }) => {
   if (resourceType == 'planets') {
     return (
       <div
-        className={`p-4 rounded-xl border border-gray-300 cursor-pointer hover:bg-gray-100 transition-colors transform duration-500 hover:text-black hover:scale-[1.02] shadow-white shadow-md ${className}`}
+        className={`p-4 rounded-xl border border-gray-300 cursor-pointer hover:bg-gray-900 transition-colors transform duration-500 hover:scale-[1.02] shadow-white hover:shadow-white shadow-md hover:shadow-lg ${className}`}
         onClick={() => onClick(resource.uid)}
       >
         <h1 className="font-bold text-2xl">
@@ -41,7 +41,7 @@ const ResourceCard = ({ resource, onClick, className = '', resourceType }) => {
   } else if (resourceType == 'people'){
      return (
       <div
-        className={`p-4 rounded-xl border border-gray-300 cursor-pointer hover:bg-gray-100 transition-colors transform duration-500 hover:text-black hover:scale-[1.02] shadow-white shadow-md ${className}`}
+        className={`p-4 rounded-xl border border-gray-300 cursor-pointer hover:bg-gray-100 transition-colors transform duration-500 hover:text-black hover:scale-[1.02] shadow-white hover:shadow-white shadow-md hover:shadow-lg ${className}`}
         onClick={() => onClick(resource.uid)}
       >
         <h1 className="font-bold text-2xl">
@@ -73,7 +73,7 @@ const ResourceCard = ({ resource, onClick, className = '', resourceType }) => {
   } else if (resourceType == 'species'){
      return (
       <div
-        className={`p-4 rounded-xl border border-gray-300 cursor-pointer hover:bg-gray-100 transition-colors transform duration-500 hover:text-black hover:scale-[1.02] shadow-white shadow-md ${className}`}
+        className={`p-4 rounded-xl border border-gray-300 cursor-pointer hover:bg-gray-100 transition-colors transform duration-500 hover:text-black hover:scale-[1.02] shadow-white hover:shadow-white shadow-md hover:shadow-lg ${className}`}
         onClick={() => onClick(resource.uid)}
       >
         <h1 className="font-bold text-2xl">
@@ -102,7 +102,7 @@ const ResourceCard = ({ resource, onClick, className = '', resourceType }) => {
   } else if (resourceType == 'starships'){
      return (
       <div
-        className={`p-4 rounded-xl border border-gray-300 cursor-pointer hover:bg-gray-100 transition-colors transform duration-500 hover:text-black hover:scale-[1.02] shadow-white shadow-md ${className}`}
+        className={`p-4 rounded-xl border border-gray-300 cursor-pointer hover:bg-gray-100 transition-colors transform duration-500 hover:text-black hover:scale-[1.02] shadow-white hover:shadow-white shadow-md hover:shadow-lg ${className}`}
         onClick={() => onClick(resource.uid)}
       >
         <h1 className="font-bold text-2xl">
@@ -125,7 +125,7 @@ const ResourceCard = ({ resource, onClick, className = '', resourceType }) => {
   } else if (resourceType == 'vehicles'){
      return (
       <div
-        className={`p-4 rounded-xl border border-gray-300 cursor-pointer hover:bg-red-100 transition-colors transform duration-500 hover:text-black hover:scale-[1.02] shadow-white shadow-md ${className}`}
+        className={`p-4 rounded-xl border border-gray-300 cursor-pointer hover:bg-red-100 transition-colors transform duration-500 hover:text-black hover:scale-[1.02] shadow-white hover:shadow-white shadow-md hover:shadow-lg ${className}`}
         onClick={() => onClick(resource.uid)}
       >
         <h1 className="font-bold text-2xl">
@@ -148,7 +148,7 @@ const ResourceCard = ({ resource, onClick, className = '', resourceType }) => {
   } else {
      return (
       <div
-        className={`p-4 rounded-xl border border-gray-300 cursor-pointer hover:bg-red-100 transition-colors transform duration-500 hover:text-black hover:scale-[1.02] shadow-white shadow-md ${className}`}
+        className={`p-4 rounded-xl border border-gray-300 cursor-pointer hover:bg-red-100 transition-colors transform duration-500 hover:text-black hover:scale-[1.02] shadow-white hover:shadow-white shadow-md hover:shadow-lg ${className}`}
         onClick={() => onClick(resource.uid)}
       >
         <h1 className="font-bold text-2xl">
